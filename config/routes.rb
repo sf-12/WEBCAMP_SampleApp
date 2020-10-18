bbd937838fb63966c607649e9dd313d64a829e82
sf-12 <s.fukasawa12@gmail.com>
2020-10-18 07:24:43 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # URL「top」にアクセスした時にhomesコントローラのtopアクションを呼び出す
+  get 'top' => 'homes#top'
+end
