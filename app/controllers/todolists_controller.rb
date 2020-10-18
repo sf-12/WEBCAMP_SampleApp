4d3a265ee2f37653202e1a95a296e5601b06c580
sf-12 <s.fukasawa12@gmail.com>
2020-10-18 13:28:32 +0000

    [Finish] 3-4

---
+++ app/controllers/todolists_controller.rb
@@ -0,0 +1,18 @@
+class TodolistsController < ApplicationController
+  def new
+    #Viewへ渡すためのインスタンス変数に空のモデルオブジェクトを生成する。
+    @list = List.new
+  end
+  
+  def create
+    list = List.new(list_params)
+    list.save
+    redirect_to '/top'
+  end
+  
+  private
+  #ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body)
+  end
+end
