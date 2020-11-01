94b8b54d6d2e70010d54598667df0c8b242a327f
sf-12 <s.fukasawa12@gmail.com>
2020-11-01 10:29:55 +0000

    [Finish] 3-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '10b5571e1558bdc3ff4f338bfdec8d9c896dc34a9354ab789ab537505377cc692855cb0a1c942aacc680491cc79a12963f5c0e9e4eb53555a0c9f10463982b2c'
