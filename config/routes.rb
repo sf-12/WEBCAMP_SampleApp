d5091949884d4459369fac813c8ff512299c8c09
sf-12 <s.fukasawa12@gmail.com>
2020-11-01 06:48:18 +0000

    [Finish] 3-5

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # URL「top」にアクセスした時にhomesコントローラのtopアクションを呼び出す
   get 'top' => 'homes#top'
   post 'todolists' => 'todolists#create'
-  get 'todolists' => 'todolists#index'  
+  get 'todolists' => 'todolists#index'
+  get 'todolists/:id' => 'todolists#show', as: 'todolist'
 end
